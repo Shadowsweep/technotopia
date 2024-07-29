05f76846f47934e0aeeec54293e3a9eedf213bb6
Shadowsweep <utkarshhg911@gmail.com>
2024-07-29 09:09:22 +0530

    file structure made

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const page = () => {
+  return (
+   <section className="w-full flex-center flex-col" >
+   <h1 className="head_text text-center" > Discover & Share
+       <br className="max-md:hidden" />
+       <span className=" orange_gradient text-center" >
+        AI-Powered Prompts
+       </span>
+       </h1>
+       <p className='desc text-center' >
+           open-source AI prompting tool to share creative prompts
+       </p>
+
+       
+   </section>
+  )
+}
+
+export default page
